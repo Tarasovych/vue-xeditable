47fae7ca4541623a2b24654128cbb8c27ee9b141
Madao-3 <chrishyman256@gmail.com>
2017-10-31 16:10:05 +0800

    优化使用

--- src/main.js
+++ src/main.js
@@ -1,16 +1,3 @@
-import Vue from 'vue'
 import Editable from './Editable.vue'
-new Vue({
-  el: '#app',
-  components: {
-    'editable': Editable
-  },
-  data: {
-    first: 'Do not go gentle into that good night',
-    second: "Do not go gentle into that good night, \n\Old age should burn and rave at close of day; \n\Rage, rage against the dying of the light.",
-    third: 1,
-    forth: 1,
-    options: [['Gold', 1], ['Silver', 2], ['Bronze', 3]],
-    fifth: 'Do not go <strong style="color: rgb(230, 0, 0);"><em>gentle</em></strong> into that good night'
-  }
-})
+
+Vue.component('editable', Editable);
